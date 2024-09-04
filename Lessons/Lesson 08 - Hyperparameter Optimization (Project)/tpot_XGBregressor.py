e1ceaecdae1ae566af99f6bddb0e17545769f875
Jeff Baggett <baggett.jeff@uwlax.edu>
2024-09-04 12:11:48 -0500

    Fall 2024

--- Lessons/Lesson 08 - Hyperparameter Optimization (Project)/tpot_XGBregressor.py
+++ Lessons/Lesson 08 - Hyperparameter Optimization (Project)/tpot_XGBregressor.py
@@ -9,8 +9,8 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=8675309)
 
-# Average CV score on the training set was: 0.974643870779841
-exported_pipeline = XGBRegressor(learning_rate=0.1, max_depth=8, min_child_weight=1, n_estimators=100, nthread=1, objective="reg:squarederror", reg_alpha=3.25, reg_lambda=4.0, subsample=0.7500000000000001)
+# Average CV score on the training set was: 0.9744238058725992
+exported_pipeline = XGBRegressor(learning_rate=0.1, max_depth=7, min_child_weight=1, n_estimators=100, objective="reg:squarederror", reg_alpha=2.75, reg_lambda=2.5, subsample=0.6500000000000001)
 # Fix random state in exported estimator
 if hasattr(exported_pipeline, 'random_state'):
     setattr(exported_pipeline, 'random_state', 8675309)
--- Lessons/Lesson 08 - Hyperparameter Optimization (Project)/tpot_optimal_pipeline.py
+++ Lessons/Lesson 08 - Hyperparameter Optimization (Project)/tpot_optimal_pipeline.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.linear_model import RidgeCV
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline, make_union
+from tpot.builtins import StackingEstimator
 from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -12,10 +13,10 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=8675309)
 
-# Average CV score on the training set was: 0.9702235067536723
+# Average CV score on the training set was: 0.9733855352987401
 exported_pipeline = make_pipeline(
-    PolynomialFeatures(degree=2, include_bias=False, interaction_only=False),
-    RidgeCV()
+    StackingEstimator(estimator=RidgeCV()),
+    ExtraTreesRegressor(bootstrap=True, max_features=0.6500000000000001, min_samples_leaf=1, min_samples_split=3, n_estimators=100)
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 8675309)
